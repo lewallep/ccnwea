2dbbd2db4ff7010ea5ee33ae43d534e38706a168
lewallep <fefifophil@hotmail.com>
2018-01-15 12:06:24 -0800

    Experiment functions to identify request_types.

--- server_pyramid.py
+++ server_pyramid.py
@@ -4,12 +4,27 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 def hello_world(request):
-	return Response('Hello %(name)s!' % request.matchdict)
+	return Response('Hello %(name)s!' % request.matchdict + 'adding a suffix')
+
+def post_resp(request):
+	print("inside post_resp.")
+	print(request.method)
+	#print(request.headers['host_info'])
+
+	for value in request.headers:
+		print(value)
+	print('\n')
+	print(request)
+	print(request.headers['accept'])
+	
+	return Response('Hello ')
 
 if __name__ == '__main__':
 	with Configurator() as config:
 		config.add_route('hello', '/hello/{name}')
 		config.add_view(hello_world, route_name='hello')
+		config.add_route('posting', '/posting/', request_method="POST")
+		config.add_view(post_resp, route_name='posting')
 		app = config.make_wsgi_app()
 	server = make_server('0.0.0.0', 9797, app)
 	server.serve_forever()
